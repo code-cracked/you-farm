ea77835c5ab13cd8cad5a9a631827777635f50ea
daniel-jebarson <danieljebarson21@gmail.com>
2023-03-28 23:51:25 +0530

    change

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -3,10 +3,10 @@ import { db } from "../../../config/db";
 import { collection, getDocs } from "firebase/firestore";
 
 export const handler = async (req, res) => {
-  const UserRef = collection(db, "users");
-  const snapshot = await UserRef.get()
-  const list = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-  res.send(list);
-  // res.status(200).json(data);
-  // res.status(200).json({ name: "John Doe" });
+  // const UserRef = collection(db, "users");
+  // const snapshot = await UserRef.get()
+  // const list = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+  // res.send(list);
+  // // res.status(200).json(data);
+  res.status(200).json({ name: "John Doe" });
 };
