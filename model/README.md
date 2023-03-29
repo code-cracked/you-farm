1c67475a196d4566e6a3fd18d57ee0424666ae29
A Dhruva Krishnama Raju <dhruvakrishnamaraju@gmail.com>
2023-03-29 15:51:35 +0530

    Add crop recommendation model api using flask

---
+++ model/README.md
@@ -0,0 +1,11 @@
+# Model
+model.py trains and saves the model to the disk.
+model.pkb the pickle model 
+
+# App
+app.py contains all the requiered for flask and to manage APIs.
+
+
+
+Procedure--
+Open command Prompt and go to given directory and then run python app.py
---
+++ model/model.py
@@ -0,0 +1,40 @@
+# Simple Linear Regression
+
+# Importing the libraries
+import numpy as np
+import pandas as pd
+import pickle
+import requests
+import json
+
+# Importing the dataset
+dataset = pd.read_csv('data.csv')
+y = dataset['label']
+X = dataset.drop(['label'], axis = 1)
+
+print("Shape of x:", X.shape)
+print("Shape of y:", y.shape)
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+print("The Shape of x train:", X_train.shape)
+print("The Shape of x test:", X_test.shape)
+print("The Shape of y train:", y_train.shape)
+print("The Shape of y test:", y_test.shape)
+
+# Fitting Simple Linear Regression to the Training set
+from sklearn.linear_model import LogisticRegression
+model = LogisticRegression()
+model.fit(X_train.values, y_train.values)
+
+# Predicting the Test set results
+y_pred = model.predict(X_test.values)
+
+# Saving model using pickle
+pickle.dump(model, open('model.pkl','wb'))
+
+# Loading model to compare the results
+modelf = pickle.load( open('model.pkl','rb'))
+print(modelf.predict([[4, 42, 1000, 123, 800, 7, 2000]]))
