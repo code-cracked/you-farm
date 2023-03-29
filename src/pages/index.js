9653b13587cf298118e41343f12029aedf258407
jessej-samuel <1234jessej@gmail.com>
2023-03-29 17:11:36 +0530

    feat: signup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,5 @@
-import { Container } from "@mui/material";
-import Head from "next/head";
+const Index = () => {
+  return <p>INdex</p>;
+};
 
-export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>YouFarm | Login</title>
-        <meta name="description" content="Register/ Signup for an account" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Container>
-        <h1>Home</h1>
-      </Container>
-    </>
-  );
-}
+export default Index;
