e3a62cda94efa017cd65d392be7dd9beffc3684d
daniel-jebarson <danieljebarson21@gmail.com>
2023-04-01 19:56:18 +0530

    Add route

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const { userRoutes } = require("./router/user");
 const { bidRoutes } = require("./router/bid");
 const { rentRoutes } = require("./router/rent");
+const { dealRoutes } = require("./router/deal");
 const bodyParser = require("body-parser");
 const logHandler = require("./middleware/log");
 app.use(express.json());
@@ -14,12 +15,11 @@ app.use(bodyParser.json());
 app.use(logHandler);
 
 const config = require("./config/index");
-const { getAllShows } = require("./controllers/bid");
 
 app.use("/user", userRoutes);
 app.use("/bid", bidRoutes);
 app.use("/rent", rentRoutes);
-app.route("/deal").get(getAllShows);
+app.use("/deal", dealRoutes);
 
 app.listen(config.port || 5000, () =>
   console.log(`Up & Running on ${config.url}`)
