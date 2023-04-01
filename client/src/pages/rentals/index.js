aeeb6fd3cb4d0bb281314700e8365becde9eac74
daniel-jebarson <danieljebarson21@gmail.com>
2023-04-02 01:16:43 +0530

    ✨ Done : Index page for rentals

--- client/src/pages/rentals/index.js
+++ client/src/pages/rentals/index.js
@@ -1,5 +1,42 @@
-const RentalIndex = () => {
-  return <></>;
+import Navbar from "@/components/Navbar";
+import { getAllDeals } from "@/utils/rents";
+import { useEffect, useState } from "react";
+
+const { default: Link } = require("next/link");
+
+const DealsIndex = () => {
+  const [data, setData] = useState([
+    {
+      name: "Loading...",
+      phno: "Loading...",
+    },
+  ]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await getAllDeals();
+      // console.log("Uhuhuhu", res);
+      setData(res);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <Navbar />
+      <Link href="/rentals/create">Create a deal</Link>
+      <ul>
+        {data.map((deal) => {
+          console.log(deal);
+          return (
+            <li key={deal.toString()}>
+              <Link href={`/rentals/${deal.createdby}`}>{deal.name}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 };
 
-export default RentalIndex;
+export default DealsIndex;
