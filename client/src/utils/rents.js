964958fe945747d2022ef29a53d06dc448023652
daniel-jebarson <danieljebarson21@gmail.com>
2023-04-02 02:45:16 +0530

    🐛 Fix in utils

--- client/src/utils/rents.js
+++ client/src/utils/rents.js
@@ -1,6 +1,6 @@
 // Get all deals from localhost:5000/deal
-export const getAllDeals = () => {
-  return fetch("http://localhost:5000/rent", {
+export const getAllDeals = async () => {
+  return await fetch("http://localhost:5000/rent", {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
@@ -9,8 +9,8 @@ export const getAllDeals = () => {
 };
 
 // Get deal by id from localhost:5000/deal/
-export const getDealById = (id) => {
-  return fetch(`http://localhost:5000/rent/${id}`, {
+export const getDealById = async (id) => {
+  return await fetch(`http://localhost:5000/rent/${id}`, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
@@ -19,8 +19,8 @@ export const getDealById = (id) => {
 };
 
 // Gets rents of the delaer from localhost:5000/rent/:phone(not implemented)
-export const getRentsOfDealer = (phone) => {
-  return fetch(`http://localhost:5000/rent`, {
+export const getRentsOfDealer = async (phone) => {
+  return await fetch(`http://localhost:5000/rent`, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
@@ -28,9 +28,27 @@ export const getRentsOfDealer = (phone) => {
   }).then((res) => res.json());
 };
 
+export const createDeal = async (rent) => {
+  return await fetch("http://localhost:5000/rent/create", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(rent),
+  }).then((res) => {
+    if (res.status !== 200) {
+      return {
+        status: res.status,
+        message: res.statusText,
+      };
+    }
+    return res.json();
+  });
+};
+
 // Post a rent to localhost:5000/rent
-export const postRent = (rent) => {
-  return fetch("http://localhost:5000/rent", {
+export const postRent = async (rent) => {
+  return await fetch("http://localhost:5000/rent", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
