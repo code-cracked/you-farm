69fc79eb7bc21477669c5f0353295a183e73fe99
daniel-jebarson <danieljebarson21@gmail.com>
2023-04-02 03:11:56 +0530

    🚀 Feat : comp id in rent

--- client/src/pages/rentals/[id].js
+++ client/src/pages/rentals/[id].js
@@ -1,10 +1,12 @@
 import Navbar from "@/components/Navbar";
+import BidsDialog from "@/components/BidsDialog";
 import { PermIdentityOutlined } from "@mui/icons-material";
-import { Container, Stack, Typography } from "@mui/material";
+import { Container, Stack, Typography, Button } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import { getDealById } from "@/utils/rents";
+import { getDealById, postRent } from "@/utils/rents";
+import { Toaster, toast } from "react-hot-toast";
 const product = {
   title: "Sickle",
   quantity: "2",
@@ -33,6 +35,41 @@ const Deals = () => {
   const router = useRouter();
   const { id } = router.query;
   const [bids, setBids] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [hasBid, setHasBid] = useState(false);
+  const [bidAmount, setBidAmount] = useState(0);
+  const handleClickOpen = () => {
+    if (hasBid)
+      toast(`You have already placed a bid of $${bidAmount} for this product`, {
+        icon: "🤔",
+      });
+    else setOpen(true);
+  };
+
+  const handleClose = async (value) => {
+    setOpen(false);
+    const userData = JSON.parse(window.localStorage.getItem("userdata"));
+    const bid = {
+      amount: value,
+      phone: userData["phone"],
+      id,
+      name: userData["name"],
+    };
+    await postRent(bid)
+      .then((res) => {
+        if (res.status == 200) {
+          setBidAmount(value);
+          toast.success(`You have placed a bid of $ ${value}`);
+          setHasBid(true);
+        } else {
+          toast.error(`Error: ${res.status}`);
+        }
+      })
+      .catch((err) => {
+        toast.error(`Error: ${err}`);
+        console.log(err);
+      });
+  };
   useEffect(() => {
     const fetchData = async () => {
       let { rents: res } = await getDealById(id);
@@ -61,6 +98,7 @@ const Deals = () => {
   return (
     <>
       <Navbar />
+      <Toaster position="bottom-right" />
       <Container>
         <Stack
           minHeight={"20vh"}
@@ -82,6 +120,12 @@ const Deals = () => {
           >
             <Typography variant="h5">{product.quantity} units</Typography>
             <Typography variant="h6">Closes in {product.close_time}</Typography>
+            <div>
+              <Button variant="contained" onClick={handleClickOpen}>
+                Bid
+              </Button>
+              <BidsDialog hasBid={hasBid} open={open} onClose={handleClose} />
+            </div>
           </Stack>
         </Stack>
         <Stack minHeight={"60vh"}>
