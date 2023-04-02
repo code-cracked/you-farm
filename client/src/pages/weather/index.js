dafaba03e06642dc278623cfc3013734adea77bb
theFutureGuy <Ashiq789git@gmail.com>
2023-04-02 17:52:48 +0530

    weaather js changed

--- client/src/pages/weather/index.js
+++ client/src/pages/weather/index.js
@@ -1,58 +1,33 @@
 import Head from "next/head";
 import { useState, useEffect } from "react";
-import Axios from "axios";
+import axios from "axios";
 
 export default function Home() {
   const [data, setData] = useState(null);
   const city = "madurai";
-  const openWeatherKey = process.env.NEXT_PUBLIC_API_KEY;
-  const weatherUrl = "https://api.openweathermap.org/data/2.5/forecast";
-  const urlToFetch = `${weatherUrl}?&q=${city}&APPID=${openWeatherKey}&units=metric`;
 
-  // async function getLocationWeather(location) {
-  //   const result = await fetch(urlToFetch);
-  //   console.log(result)
-  //   return result.json();
-  // }
-  // getLocationWeather("London");
+  
+  const urlToFetch = `http://localhost:5000/weather?lat=1&long=1`;
 
-  // const { data: output } = await Axios.get(
-  //   "http://localhost:5000/user/7395879437"
-  // ).then();
 
-  // console.log(output);
-  // const keys = Object.keys(output);
-  // const lat = output[keys][0].location.lat;
-  // const lon = output[keys][0].location.long;
-  // fetch(
-  //   `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=52e635d515f603126f9f17a31554fc92`
-  // )
-  //   .then((res) => res.json())
-  //   .then((result) => {
-  //     console.log(result);
-  //     const { temp, humidity } = result.main;
-  //     let tempn = parseInt(temp);
-  //     let humidn = parseInt(humidity);
-  //   });
-  const getData = async () => {
-    await fetch(urlToFetch)
-      .then((res) => res.json())
-      .then((result) => {
-        console.log(result);
-        setData(result);
-      });
-  };
+  
 
   useEffect(() => {
+    const lon = window.localStorage.getItem('location.latitude');
+    const lan = window.localStorage.getItem('location.longitude');
+
+    const getData =  async() => {
+      axios.get(urlToFetch).then((res) => setData(res.data))
+   };
     getData();
   }, []);
 
   return (
     <div>
-      {data !== null && data.list !== undefined ? (
+      {data !== null ? (
         <div>
-          {data.list.map((obj) => {
-            return <div>{JSON.stringify(obj.main)}</div>;
+          {data.map((obj) => {
+            return <div>{JSON.stringify(obj)}</div>;
           })}
         </div>
       ) : (
--- server/router/weather.js
+++ server/router/weather.js
@@ -1,31 +1,30 @@
 const express = require('express');
 const router = express.Router();
-const fetch = require('fetch');
-//import {fetch} from "node-fetch";
+// const fetch = require('fetch');
+const Axios =require("axios")
+// import {fetch} from "node-fetch";
 
 const getDaysWeather = (list) => {
 	let result = [];
 	list.map((day, index) => {
 		if (index%8 === 0)
-		result.push(day.weather.main);
+		result.push(day.weather[0]);
 	});
 	return result;
 }
-router.route('/').get((req, res) => {
-	// Get lat and long from body
+router.route('/').get(async(req, res) => {
+	
 	const lat = req.query.lat;
 	const long = req.query.long;
-
+	const APIKEY="52e635d515f603126f9f17a31554fc92"
     // console.log("helloo")
 
 	// Get weather from lat and long
-	fetch.fetchUrl(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&units=metric&appid=52e635d515f603126f9f17a31554fc92&cnt=40`,(error,meta,body) => {res.json(JSON.parse(body.toString()).list)})
+	const {data}=await Axios.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&units=metric&appid=${APIKEY}`)
 		// .then(response => response.json())
-		// .then(data => {
-		// 	res.json(getDaysWeather(data.list))
-		// })
-
-    // res.send("hello");
+	
+	console.log(data.list)
+    res.send(getDaysWeather(data.list));
 });
 
 module.exports = {
