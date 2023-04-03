c797d819aff0d772682039ee8ceb6e3b8894af90
theFutureGuy <Ashiq789git@gmail.com>
2023-04-03 12:46:53 +0530

    Card MAterial UI added

--- client/src/pages/weather/index.js
+++ client/src/pages/weather/index.js
@@ -1,16 +1,29 @@
 import Head from "next/head";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import * as React from 'react';
+import {
+  Box,
+  Card,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+  Avatar,
+  CardHeader,
+  IconButton,
+} from "@material-ui/core";
+import { Theme, createStyles, makeStyles } from "@material-ui/core/styles";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import Grid from '@mui/material/Grid';
+
+
 
 export default function Home() {
   const [data, setData] = useState(null);
   const city = "madurai";
 
-  
   const urlToFetch = `http://localhost:5000/weather?lat=1&long=1`;
-
-
-  
 
   useEffect(() => {
     const lon = window.localStorage.getItem('location.latitude');
@@ -22,17 +35,36 @@ export default function Home() {
     getData();
   }, []);
 
+
   return (
-    <div>
-      {data !== null ? (
-        <div>
-          {data.map((obj) => {
-            return <div>{JSON.stringify(obj)}</div>;
-          })}
-        </div>
-      ) : (
-        ""
-      )}
-    </div>
+    data==null?"":  data.map((obj) => {
+      return(
+    //         <div>
+    //           <Card lg={{ maxWidth: 345 }}>
+    //               <CardActionArea>
+    //                   <CardContent>
+    //                     <Typography variant="body2" color="text.secondary"> 
+    //                       {JSON.stringify(obj.main)}
+    //                     </Typography>
+    //                   </CardContent> 
+    //                 </CardActionArea>
+    //               </Card>
+    //         </div>
+    //     )})  
+    <Grid container spacing={2}>
+    <Card style={{ width: '18rem' }}>
+      <Card.Header></Card.Header>
+      {/* <Card.Img variant="top" src={""} /> */}
+      <Card.Body>
+        <Card.Title>Monday</Card.Title>
+        <Card.Text>
+        {JSON.stringify(obj.main)}
+        </Card.Text>
+        <Button variant="primary">Add to cart</Button>
+        <Button>Add to favs</Button>
+      </Card.Body>
+    </Card>
+    </Grid>
+    )})
   );
 }
--- server/router/weather.js
+++ server/router/weather.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
-// const fetch = require('fetch');
 const Axios =require("axios")
-// import {fetch} from "node-fetch";
+
 
 const getDaysWeather = (list) => {
 	let result = [];
