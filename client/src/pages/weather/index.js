598ee7e78696b9e72c2d2f4db902dfbff7e0fcba
theFutureGuy <Ashiq789git@gmail.com>
2023-04-05 00:12:42 +0530

    Weather Done

--- client/src/pages/weather/index.js
+++ client/src/pages/weather/index.js
@@ -1,28 +1,13 @@
-<<<<<<< HEAD
-=======
-import Head from "next/head";
->>>>>>> 25f065ee1735d026b1314fd2e947adaaeb3e314d
 import Navbar from "@/components/Navbar";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import * as React from 'react';
 import Box from "@mui/material/Box";
-<<<<<<< HEAD
 import CardContent from "@mui/material/CardContent";
 import CardHeader from '@mui/material/CardHeader';
 import Typography from "@mui/material/Typography";
 import { createTheme } from "@mui/material/styles";
 import Container from "@mui/material/Container";
-=======
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardHeader from '@mui/material/CardHeader';
-import Typography from "@mui/material/Typography";
-import { Grid } from "@mui/material";
-
-// import bgImg from "./images/bg-img.jpg";
-
->>>>>>> 25f065ee1735d026b1314fd2e947adaaeb3e314d
 
 const theme = createTheme();
 
@@ -45,100 +30,8 @@ export default function Home() {
    setUserData(JSON.parse(window.localStorage.getItem("userdata")))
      getData();
   }, []);
-<<<<<<< HEAD
   
   return (
-=======
-
-  
-  return (
-<div sx={{
-    root: {
-      marginTop: 50,
-      display: "flex",
-      width: 550,
-      height: 250,
-    },
-    cardcss: {
-      // backgroundImage: "url(" + bgImg + ")",
-      backgroundPosition: "center",
-    },
-  }}>
-    <Navbar />
-      <CardHeader
-         title={'Weather forecast'}
-         style={{textAlign:'center',paddingBottom:'10%'}}
-      />
-     
-      <Grid
-          container
-          spacing={3}
-          direction="row"
-          justify="flex-start"
-          alignItems="flex-start"
-            >
-                {data.map(elem => (
-                  console.log(elem.date),
-                    <Grid item xs={5} sm={5} md={5} key={data.indexOf(elem)} maxWidth={200}>
-                        <Card>
-                            <CardHeader
-                                title={`Day : ${new Date(elem.date).toLocaleString()}`}
-                                subheader={`lat : ${lat} lon: ${lon}`}
-                            />
-                            <CardContent>
-                            <Box display="flex" flexDirection="row-reverse">
-          <Box>
-            <Typography variant="h4" color="textPrimary">
-              Temp Max: {elem.temp_max}
-              <span>&#176;</span>
-              {"C"}
-            </Typography>
-            <Typography variant="h4" color="textPrimary">
-              Temp Min: {elem.temp_min}
-              <span>&#176;</span>
-              {"C"}
-            </Typography>
-          </Box>
-        </Box>
-      </CardContent>
-      <CardContent>
-        <Box display="flex" flexDirection="row-reverse">
-          <Box p={0}>
-            <Typography variant="h6" color="textSecondary">
-              {elem.weather.description}
-            </Typography>
-          </Box>
-        </Box>
-      </CardContent>
-      <CardContent>
-        <Box display="flex" flexDirection="row">
-          <Box p={1}>
-            <Typography variant="h6" color="textPrimary">
-              Humidity: {elem.humidity} %
-            </Typography>
-          </Box>
-          <Box p={1}>
-            <Typography variant="h6" color="textPrimary">
-              pressure: {elem.pressure} pa
-            </Typography>
-          </Box>
-          <Box p={1}>
-            <Typography variant="h6" color="textPrimary">
-              wind: {elem.speed} km/h
-            </Typography>
-          </Box>
-        </Box>
-      </CardContent>
-      </Card>
-      </Grid>
-     ))}
-   </Grid>
-  </div>
-);
-  
-}
-{/* <div className={classes.root}> <Grid item xs={12} sm={6} md={3}>
->>>>>>> 25f065ee1735d026b1314fd2e947adaaeb3e314d
     <div>
       <Navbar />
       <Typography
