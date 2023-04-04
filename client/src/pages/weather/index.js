6cd3e05ce5a243f874e5934b30fbd080cd680718
A Dhruva Krishnama Raju <48171972+DeeKay02@users.noreply.github.com>
2023-04-05 00:00:51 +0530

    Revert "☁ Weather page Done"
    This reverts commit 07e8a4fae01bae2310f02a525ac66741d6ef2588.

--- client/src/pages/weather/index.js
+++ client/src/pages/weather/index.js
@@ -1,125 +1,61 @@
-import Navbar from "@/components/Navbar";
+import Head from "next/head";
 import { useState, useEffect } from "react";
-import axios from "axios";
-import * as React from 'react';
-import Box from "@mui/material/Box";
-import CardContent from "@mui/material/CardContent";
-import CardHeader from '@mui/material/CardHeader';
-import Typography from "@mui/material/Typography";
-import { createTheme } from "@mui/material/styles";
-import Container from "@mui/material/Container";
-
-const theme = createTheme();
-
+import Axios from "axios";
 export default function Home() {
+  // const [data, setData] = useState(null);
+  // const city = "madurai";
+  // const openWeatherKey = process.env.NEXT_PUBLIC_API_KEY;
+  // const weatherUrl = "https://api.openweathermap.org/data/2.5/forecast";
+  // const urlToFetch = `${weatherUrl}?&q=${city}&APPID=${openWeatherKey}&units=metric`;
 
-  const [data,setData]=useState([])  
-  const [userData,setUserData]=useState({
-    "location": {"latitude": 1, "longitude": 1}
-  })
+  // // async function getLocationWeather(location) {
+  // //   const result = await fetch(urlToFetch);
+  // //   console.log(result)
+  // //   return result.json();
+  // // }
+  // // getLocationWeather("London");
 
-  useEffect(() => {
-    const getData =  async() => {
-      const urlToFetch = `http://localhost:5000/weather?lat=${userData["location"]["latitude"]}&long=${userData["location"]["latitude"]}`;
-     await axios.get(urlToFetch).then((res) => {
-        setData(res.data);
-        console.log(res.data)
-        
-      })
-   };
-   setUserData(JSON.parse(window.localStorage.getItem("userdata")))
-     getData();
-  }, []);
-  
-  return (
-    <div>
-      <Navbar />
-      <Typography
-        component="h1"
-        variant="h4"
-        paddingLeft={5}
-        paddingTop={1}
-        marginY={2}
-        fontWeight={theme.typography.fontWeightBold}
-        textAlign={"center"}
-      >
-        5 Days Weather Forecast
-      </Typography>
-      <Container
-        component="main"
-        maxWidth="xl"
-        sx={{
-          display: "flex",
-          flexDirection: "row",
-          marginTop: 1,
-          flexShrink: 0,
-          flexWrap: "wrap",
-          justifyContent: "center",
-        }}
-      >
-        {data.map(elem => (
-          <Box
-            minWidth={200}
-            maxWidth={400}
-            margin={2}
-            boxShadow={5}
-            padding={2}
-            borderRadius={4}
-            display={"flex"}
-            flexDirection={"column"}
-            justifyContent={"center"}
-          >
-              <CardHeader
-                title={`Date : ${new Date(elem.date).toLocaleString()}`}
-                subheader={`lat : ${userData["location"]["latitude"]} lon: ${userData["location"]["longitude"]}`}
-              />
-              <CardContent>
-              <Box display="flex" flexDirection="row-reverse">
-          <Box>
-            <Typography variant="h5" color="textPrimary">
-              Temp Max: {elem.temp_max}
-              <span>&#176;</span>
-              {"C"}
-            </Typography>
-            <Typography variant="h5" color="textPrimary">
-              Temp Min: {elem.temp_min}
-              <span>&#176;</span>
-              {"C"}
-            </Typography>
-          </Box>
-        </Box>
-      </CardContent>
-      <CardContent>
-        <Box display="flex" flexDirection="row-reverse">
-          <Box p={0}>
-            <Typography variant="h6" color="textSecondary">
-              {elem.weather.description}
-            </Typography>
-          </Box>
-        </Box>
-      </CardContent>
-      <CardContent>
-        <Box display="flex" flexDirection="row">
-          <Box p={1}>
-            <Typography variant="h6" color="textPrimary">
-              Humidity: {elem.humidity} %
-            </Typography>
-          </Box>
-          <Box p={1}>
-            <Typography variant="h6" color="textPrimary">
-              Pressure: {elem.pressure} pa
-            </Typography>
-          </Box>
-          <Box p={1}>
-            <Typography variant="h6" color="textPrimary">
-              Wind: {elem.speed} km/h
-            </Typography>
-          </Box>
-        </Box>
-      </CardContent>
-      </Box>
-      ))}
-        </Container>
-    </div>
-  ); 
+  // // const { data } = Axios.get("http://localhost:5000/user/7395879437").then();
+
+  // console.log(data);
+
+  // // const lat = data[0].location.lat;
+  // // const lon = data[0].location.long
+  // fetch(
+  //   `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=52e635d515f603126f9f17a31554fc92`
+  // )
+  //   .then((res) => res.json())
+  //   .then((data) => {
+  //     // console.log(data);
+  //     const { temp, humidity } = data.main;
+  //     tempn = parseInt(temp);
+  //     humidn = parseInt(humidity);
+  //   });
+  // const getData = async () => {
+  //   await fetch(urlToFetch)
+  //     .then((res) => res.json())
+  //     .then((result) => {
+  //       console.log(result);
+  //       setData(result);
+  //     });
+  // };
+
+  // useEffect(() => {
+  //   getData();
+  // }, []);
+
+  // return (
+  //   <div>
+  //     {data !== null ? (
+  //       <div>
+  //         {data.list.map((obj) => {
+  //           return <div>{JSON.stringify(obj.main)}</div>;
+  //         })}
+  //       </div>
+  //     ) : (
+  //       ""
+  //     )}
+  //   </div>
+  // );
+  return <></>;
 }
